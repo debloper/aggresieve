e1941b2495ba5225eeb162d9714baca287124c7d
Soumya Deb <debloper@gmail.com>
2014-01-16 21:06:47 +0530

    Proof of concept feedreader-API service
    Signed-off-by: Soumya Deb <debloper@gmail.com>

--- app.js
+++ app.js
@@ -6,8 +6,19 @@ app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
 
 app.use(flatiron.plugins.http);
 
+var Feedr = require("feedr").Feedr;
+var feedr = new Feedr();
+
 app.router.get('/', function () {
-  this.res.json({ 'status': 'OK' })
+  this.res.json({ 'status': '200 OK' })
+});
+
+app.router.get('/feeds', function () {
+  var feedUrl = "https://github.com/debloper/aggresieve/commits/master.atom";
+  var that = this;
+  feedr.readFeed(feedUrl, {}, function (error, data, headers) {
+    that.res.json(data);
+  });
 });
 
 app.start(3000);
